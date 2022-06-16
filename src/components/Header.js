5c6922053356bfd80b642a70279f15c8410912b6
Ytar99 <ytar.99@yandex.ru>
2022-06-16 19:10:12 +0300

    Complete header

---
+++ src/components/Header.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { MenuOutlined, Close } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import './styles/Header.scss';
+
+const Header = () => {
+  const [active, setActive] = useState(false);
+
+  const showMenu = () => {
+    setActive(true);
+  };
+
+  return (
+    <div className="header">
+      <div className="header__logo">
+        <h1>Ytar</h1>
+      </div>
+      <nav
+        className={
+          active ? 'header__navbar active' : 'header__navbar'
+        }
+      >
+        <ul className="ul-items">
+          <div className="closed">
+            <Close
+              className="close"
+              onClick={() => setActive(false)}
+            />
+          </div>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="#">About</Link>
+          </li>
+          <li>
+            <Link to="#">Portfolio</Link>
+          </li>
+          <li>
+            <Link to="#">Pages</Link>
+          </li>
+          <li>
+            <Link to="#">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="changer">
+        <MenuOutlined
+          className="menu"
+          onClick={() => setActive(true)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles/Main.scss';
+
+const Main = () => {
+  return <div>Main</div>;
+};
+
+export default Main;
